5f5c4a29a9b100c25b41fe3ce1a99baec8a929ab
Daniel Szopa <danjszopa@gmail.com>
2026-04-27 22:58:47 -0500

    Configure gitsigns

---
+++ nvim/.config/nvim/plugin/gitsigns.lua
@@ -0,0 +1,8 @@
+vim.pack.add({
+  Repo.gh("lewis6991/gitsigns.nvim"),
+})
+
+require("gitsigns").setup()
+
+vim.keymap.set("n", "]h", function() require("gitsigns").nav_hunk("next") end, { desc = "Go to next git hunk" })
+vim.keymap.set("n", "[h", function() require("gitsigns").nav_hunk("prev") end, { desc = "Go to previous git hunk" })
