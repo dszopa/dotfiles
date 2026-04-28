c288eb8b950317a307ff03a0459ecd31e90271d5
Daniel Szopa <danjszopa@gmail.com>
2026-04-27 22:58:36 -0500

    Show hidden files in neo-tree

--- nvim/.config/nvim/plugin/neo-tree.lua
+++ nvim/.config/nvim/plugin/neo-tree.lua
@@ -11,6 +11,16 @@ vim.pack.add({
   -- Repo.gh('s1n7ax/nvim-window-picker'), -- Configured in `nvim-window-picker.lua`
 })
 
+require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_hidden = false
+    }
+  }
+})
+
 vim.keymap.set("n", "<leader>tt", "<cmd>Neotree toggle<CR>", { desc = "Toggle Neo-Tree" })
 vim.keymap.set("n", "<leader>to", "<cmd>Neotree show<CR>", { desc = "Open Neo-Tree" })
 vim.keymap.set("n", "<leader>tc", "<cmd>Neotree close<CR>", { desc = "Close Neo-Tree" })
