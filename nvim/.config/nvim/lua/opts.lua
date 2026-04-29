e0b089097f1eac57919f94a391e48c20b58dd75a
Daniel Szopa <danjszopa@gmail.com>
2026-04-28 23:30:18 -0500

    nvim - Set a bunch of options for cleaner default behavior

--- nvim/.config/nvim/lua/opts.lua
+++ nvim/.config/nvim/lua/opts.lua
@@ -16,6 +16,9 @@ vim.opt.updatetime = 4000
 -- Ignore case when searching patterns.
 vim.opt.ignorecase = true
 
+-- Override the 'ignorecase' option if the search pattern contains uppercase letters.
+vim.opt.smartcase = true
+
 -- Automatically switch to case-sensitive search if a capital letter is used.
 vim.opt.smartcase = true
 
@@ -49,3 +52,48 @@ vim.opt.listchars = { tab = "»·", trail = "•" }
 
 -- Only show the tabline when there are at least two tabs.
 vim.opt.showtabline = 1
+
+-- Always leave 8 lines above / below cursor
+vim.opt.scrolloff = 8
+
+-- Highlight the 80th column to encourage keeping lines within 80 characters.
+vim.opt.colorcolumn = "80"
+
+-- Highlight the current line.
+vim.opt.cursorline = true
+
+-- Do not highlight search matches after the search is done.
+vim.opt.hlsearch = false
+
+-- Show search matches as you type, but do not highlight them after the search is done.
+vim.opt.incsearch = true
+
+-- When opening a new split, place it below or to the right of the current window.
+vim.opt.splitbelow = true
+
+-- When opening a new vertical split, place it to the right of the current window.
+vim.opt.splitright = true
+
+-- Enable spell checking.
+vim.opt.spell = true
+
+-- Set the language for spell checking to English (United States).
+vim.opt.spelllang = "en_us"
+
+-- Reduce the time it takes for CursorHold to trigger, which is used by some plugins for features like showing diagnostics or triggering autocommands.
+vim.opt.updatetime = 250
+
+-- Reduce the time it takes for mapped key sequences to complete, which can make the editor feel more responsive when using key mappings.
+vim.opt.timeoutlen = 300
+
+-- Don't show mode in the status line.
+vim.opt.showmode = false
+
+-- Automatically reload files that have been changed outside of Neovim.
+vim.opt.autoread = true
+
+-- Insert mode completion behavior:
+-- 1. Show the completion menu even if there is only one match
+-- 2. Do not insert text until a selection is made
+-- 3. Show the completion menu as a popup
+vim.opt.completeopt = { "menuone", "popup", "noinsert" }
