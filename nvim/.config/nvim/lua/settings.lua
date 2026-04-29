8a2b569f2a0caa428d7964a8d48e74c671b738d5
Daniel Szopa <danjszopa@gmail.com>
2026-04-28 23:20:43 -0500

    nvim - Add some diagnostic / back up settings

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -1,4 +1,21 @@
 -- This file houses configuration settings that are intended to be ran
 -- after plugins have been loaded.
+--
+-- It serves as a home for nvim settings that are not related to plugins, or standalone opts.
 
 vim.cmd.colorscheme("catppuccin")
+
+-- Show diagnostics as virtual lines instead of virtual text, but only for the current line.
+vim.diagnostic.config({
+  virtual_lines = {
+    current_line = true,
+  },
+})
+
+-- Create the backup directory if it doesn't already exist.
+local backup_dir = vim.fn.expand("~/.vim/backup")
+if vim.fn.isdirectory(backup_dir) == 0 then
+  vim.fn.mkdir(backup_dir, "p")
+end
+-- Set the backup directory to `backup_dir` & create a unique filename for each backup file.
+vim.opt.backupdir = backup_dir .. "//"
