ab0be6a96542353c2d3d258ebf3452093d8601e6
Daniel Szopa <danjszopa@gmail.com>
2026-05-09 16:07:39 -0500

    nvim - Configure dartls
    Big update here is to exclude the pubcache from analysis, everything
    else is minor

--- nvim/.config/nvim/lua/lsp/dart.lua
+++ nvim/.config/nvim/lua/lsp/dart.lua
@@ -1,1 +1,26 @@
+-- Options from: https://github.com/dart-lang/sdk/blob/6d6b2ae57f6d6e592fd0d4544ca3d93b71b1f2da/pkg/analysis_server/tool/lsp_spec/README.md
+vim.lsp.config("dartls", {
+  init_options = {
+    onlyAnalyzeProjectsWithOpenFiles = true,
+    closingLabels = true,
+    outline = true,
+    flutterOutline = false,
+    allowOpenUri = true,
+  },
+  settings = {
+    dart = {
+      analysisExcludedFolders = {
+        vim.fn.expand("$HOME/.pub-cache"),
+      },
+      enableSdkFormatter = false,
+      completeFunctionCalls = true,
+      showTodos = true,
+      renameFilesWithClasses = "prompt",
+      enableSnippets = true,
+      updateImportsOnRename = true,
+      inlayHints = true,
+    },
+  },
+})
+
 vim.lsp.enable("dartls")
