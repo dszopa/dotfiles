f80f7b445b470bfcb3ba9570796739647cfac5db
Daniel Szopa <danjszopa@gmail.com>
2026-05-09 16:15:22 -0500

    nvim - Configure eslint lsp

--- nvim/.config/nvim/lua/lsp/eslint.lua
+++ nvim/.config/nvim/lua/lsp/eslint.lua
@@ -1,1 +1,25 @@
+--- See [vscode-eslint](https://github.com/microsoft/vscode-eslint/blob/55871979d7af184bf09af491b6ea35ebd56822cf/server/src/eslintServer.ts#L216-L229) for configuration options.
+vim.lsp.config("eslint", {
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "typescript",
+    "typescriptreact",
+    "vue",
+    "svelte",
+    "astro",
+    "htmlangular",
+    "yaml",
+    "json",
+    "jsonc",
+    "markdown",
+    "toml",
+    "xml",
+    "html",
+    "css",
+    "scss",
+    "pcss",
+  },
+})
+
 vim.lsp.enable("eslint")
