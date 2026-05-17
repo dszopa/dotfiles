9b7004617ec3c0068fd553139f1e647d94fdc116
Daniel Szopa <danjszopa@gmail.com>
2026-05-17 17:46:34 -0500

    tmux - Fix bug where "S" picked the wrong session name

--- tmux/.config/tmux/local_plugins/tmux_session_manager/pick_directory_create_and_switch_to_session.sh
+++ tmux/.config/tmux/local_plugins/tmux_session_manager/pick_directory_create_and_switch_to_session.sh
@@ -50,7 +50,7 @@ if [ "$exit_code" -ne 0 ] || [ -z "${choice:-}" ]; then
   exit 0
 fi
 
-session_name="$choice"
+session_name="$(basename "$choice")"
 
 if ! tmux has-session -t "$session_name" 2>/dev/null; then
   tmux new-session -d -s "$session_name" -c "$base_dir/$choice"
